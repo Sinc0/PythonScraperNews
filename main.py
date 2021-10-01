e8f5962875e1a4201f9fd1438be6cc1efac3c373
Sinco <sinco.developer@gmail.com>
2021-10-01 10:42:52 +0200

    Multiple - added createNewsCard() - added createTitleCard() - added saveToFavorites() - added year_progress() text to window title - improved kivy button styling - fixed year_progress() not working correctly - minor structural improvements - minor formatting improvements

--- main.py
+++ main.py
@@ -18,8 +18,11 @@ from kivy.uix.button import Button
 from kivy.config import Config
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.screenmanager import NoTransition
+from kivy.uix.boxlayout import BoxLayout
+from kivy.utils import get_color_from_hex
+# from kivy.graphics import Rectangle, Color
+# from kivy.utils import rgba
 # from kivy.uix.gridlayout import GridLayout
-# from kivy.uix.boxlayout import BoxLayout
 # from kivy.uix.textinput import TextInput
 # from kivy.uix.button import Button
 
@@ -43,7 +46,7 @@ youtubeVideos = []
 youtubeVideoCounter = 0
 
 ### global functions ###
-def fetch_youtube_channel(url, self):
+def fetch_youtube_channel(url, self, name):
     #null check
     if url == "":
         print("youtube channel is null")
@@ -87,6 +90,10 @@ def fetch_youtube_channel(url, self):
         requestResultText = str(requestResultText).replace("\\u0026", "&")
         regexYoutubeVideos = re.findall(r'"title":{"runs":\[{"text":"[^.]*"}],"[^.]*"publishedTimeText":{"simpleText":[^.]*ago"', requestResultText)
 
+        #create title card
+        bl = StartingScreen.createTitleCard(self, name, 'youtube')
+        self.ids.boxLayoutPost.add_widget(bl)
+
         for videoTitle in regexYoutubeVideos[:numberOfVideosLimit]:
             youtubeVideoCounter += 1
 
@@ -111,18 +118,24 @@ def fetch_youtube_channel(url, self):
             formatedTitle = formatTitleStep6
 
             #print youtube data to console
-            if youtubeVideoCounter < 10:
+            if youtubeVideoCounter <= numberOfVideosLimit:
                 # print(" " + str(youtubeVideoCounter) + ". " + str(formatedTitle) + " - " + str(formatedUploadDate))
                 # print("#" + str(youtubeVideoCounter) + " - " + "youtube" + " - " + str(formatedUploadDate) + " - " + str(formatedTitle))
+                # self.ids.svLabel.text += "\n" + "youtube" + " - " + str(formatedUploadDate) + ": " + str(formatedTitle) + "\n"
+                # self.ids.svScrollBar.scroll_y = 0
+                
+                #create news card
+                time.sleep(0.1)
                 print(" " + "youtube" + " - " + str(formatedUploadDate) + " - " + str(formatedTitle))
-                self.ids.svLabel.text += "\n" + "youtube" + " - " + str(formatedUploadDate) + ": " + str(formatedTitle) + "\n"
-                self.ids.svScrollBar.scroll_y = 0
+                cardText = "Youtube" + " - " + str(formatedUploadDate) + "\n" + str(formatedTitle)
+                bl = StartingScreen.createNewsCard(self, cardText, 'youtube', name)
+                self.ids.boxLayoutPost.add_widget(bl)
             # else:
             #     print(str(youtubeVideoCounter) + ". " + str(formatedTitle) + " - " + str(formatedUploadDate))
     except:
         print("youtube channel does not exist")
 
-def fetch_twitter_profile(username, self):
+def fetch_twitter_profile(username, self, name):
     #null check
     if username == "":
         print("twitter username is null")
@@ -148,9 +161,15 @@ def fetch_twitter_profile(username, self):
             tCounter += 1
             # print("#" + str(tCounter) + " - " + t.username + " - " + t.datestamp + " - " + t.tweet)
             # print("#" + str(tCounter) + " - " + "twitter" + " - " + t.datestamp + " - " + t.tweet)
+            # self.ids.svLabel.text += "\n" + "twitter" + " - " + t.datestamp.replace("-", "/") + ": " + t.tweet + "\n"
+            # self.ids.svScrollBar.scroll_y = 0
+            
+            #create news card
+            time.sleep(0.1)
             print(" " + "twitter" + " - " + t.datestamp + " - " + t.tweet)
-            self.ids.svLabel.text += "\n" + "twitter" + " - " + t.datestamp.replace("-", "/") + ": " + t.tweet + "\n"
-            self.ids.svScrollBar.scroll_y = 0
+            cardText = "Twitter" + " - " + t.datestamp.replace("-", "/") + "\n" + t.tweet
+            bl = StartingScreen.createNewsCard(self, cardText, 'twitter', name)
+            self.ids.boxLayoutPost.add_widget(bl)
         
         # print(str(len(userTweets)))
         # print("")
@@ -163,7 +182,7 @@ def year_progress():
     FEB = 31 + 28
     MAR = 31 + 28 + 31
     APR = 31 + 28 + 31+ 30
-    May = 31 + 28 + 31+ 30 + 31
+    MAY = 31 + 28 + 31+ 30 + 31
     JUN = 31 + 28 + 31+ 30 + 31 + 30
     JUL = 31 + 28 + 31+ 30 + 31 + 30 + 31
     AUG = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31
@@ -179,27 +198,47 @@ def year_progress():
     #check if leap year
     if year == "2024": 
         totalDaysThisYear = 366
-
-    #check day of year
-    if month == 9:
-        dayOfTheYear = AUG + day
-
-    #check percentage of year
-    percentageOfYear = dayOfTheYear / totalDaysThisYear
     
     #check month name
-    if month == 1: month = "JAN"
-    if month == 2: month = "FEB"
-    if month == 3: month = "MAR"
-    if month == 4: month = "APR"
-    if month == 5: month = "MAY"
-    if month == 6: month = "JUN"
-    if month == 7: month = "JUL"
-    if month == 8: month = "AUG"
-    if month == 9: month = "SEP"
-    if month == 10: month = "OCT"
-    if month == 11: month = "NOV"
-    if month == 12: month = "DEC"
+    if month == 1: 
+        dayOfTheYear = JAN + day
+        month = "JAN"
+    if month == 2: 
+        dayOfTheYear = FEB + day
+        month = "FEB"
+    if month == 3:
+        dayOfTheYear = MAR + day 
+        month = "MAR"
+    if month == 4:
+        dayOfTheYear = APR + day
+        month = "APR"
+    if month == 5:
+        dayOfTheYear = MAY + day
+        month = "MAY"
+    if month == 6:
+        dayOfTheYear = JUN + day
+        month = "JUN"
+    if month == 7:
+        dayOfTheYear = JUL + day
+        month = "JUL"
+    if month == 8:
+        dayOfTheYear = AUG + day
+        month = "AUG"
+    if month == 9:
+        dayOfTheYear = SEP + day
+        month = "SEP"
+    if month == 10:
+        dayOfTheYear = OCT + day
+        month = "OCT"
+    if month == 11:
+        dayOfTheYear = NOV + day 
+        month = "NOV"
+    if month == 12:
+        dayOfTheYear = DEC + day 
+        month = "DEC"
+            
+    #check percentage of year
+    percentageOfYear = dayOfTheYear / totalDaysThisYear
     
     print(str(day) + " " + str(month) + " " + str(year) + " - " + str(dayOfTheYear) + "/" + str(totalDaysThisYear) + " - " + str(percentageOfYear)[2:4] + "%")
     return str(day) + " " + str(month) + " " + str(year) + " - " + str(dayOfTheYear) + "/" + str(totalDaysThisYear) + " - " + str(percentageOfYear)[2:4] + "%"
@@ -209,7 +248,6 @@ def add_profile(self, name, youtube = None, twitter = None):
     profiles = []
     totalProfiles = 0
 
-    #check profile naming errors
     try: 
         #fetch all saved profiles from profiles.json if exists
         file = open('profiles.json', "r")
@@ -284,15 +322,20 @@ def fetch_news_feed(name, self):
     file = open('profiles.json', "r")
     profiles = json.load(file)
     totalProfiles = len(profiles)
-    
+
+    #clear all news card widgets
+    self.ids.boxLayoutPost.clear_widgets()
+
+    #fetch profile data
     for p in profiles:
         if p['name'] == name:
             print(p['name'])
-            self.ids.svLabel.text = ""
-            self.ids.svLabel.text = p['name']
-            self.ids.svScrollBar.scroll_y = 0
-            fetch_youtube_channel(p['youtube'], self)
-            fetch_twitter_profile(p['twitter'], self)
+            # self.ids.svLabel.text = ""
+            # self.ids.svLabel.text = p['name']
+            # self.ids.svScrollBar.scroll_y = 0
+            
+            fetch_youtube_channel(p['youtube'], self, name)
+            fetch_twitter_profile(p['twitter'], self, name)
 
     print("")
 
@@ -384,17 +427,6 @@ Config.set('graphics', 'resizable', 0)
 class StartingScreen(Screen):
     def __init__(self, **var_args):
         super(StartingScreen, self).__init__(**var_args) # that has been overwritten in a class object. to inherited methods from a parent or sibling class super function can be used to gain access
-        
-        #variables
-        savedProfiles = fetch_saved_profiles()
-        yearProgress = year_progress().replace(" - ", "\n- ")
-
-        #add welcome message
-        self.lb1.text = "Year Progress" + "\n"
-        self.lb1.text += "- " + yearProgress + "\n\n"
-        self.lb1.text += "Saved Profiles"
-        for p in savedProfiles:
-            self.lb1.text += "\n" + "- " + p['name']
 
     def on_pre_enter(self, *args):
         print("StartingScreen")
@@ -408,9 +440,10 @@ class StartingScreen(Screen):
         self.bl1.clear_widgets()
 
         #create add and edit buttons
-        btnAdd = Button(text = "+", size_hint_y = None)
+        btnBackgroundColor = get_color_from_hex("#292f33")
+        btnAdd = Button(text = "+", size_hint_y = None, background_color = btnBackgroundColor, background_normal = 'transparent', background_down = 'transparent', font_size = 30)
         btnAdd.bind(on_press=lambda *args: changeScreenToAdd(self))
-        btnEdit = Button(text = "-", size_hint_y = None)
+        btnEdit = Button(text = "-", size_hint_y = None, background_color = btnBackgroundColor, background_normal = 'transparent', background_down = 'transparent', font_size = 49)
         btnEdit.bind(on_press=lambda *args: changeScreenToEdit(self))
 
         #add create and edit buttons
@@ -483,6 +516,186 @@ class StartingScreen(Screen):
 
             #add button
             self.bl2.add_widget(newButton)
+
+    def saveToFavorites(self, cardId, profile, platform, cardText):
+        #find boxlayout widget
+        for c in self.ids.boxLayoutPost.children:
+            #find button widget
+            for s in c.children:
+                try:
+                    if s.id == cardId:
+                        if s.text == '+':
+                            #change button style
+                            s.background_color = "darkred"
+                            s.background_normal = "darkred"
+                            s.text = '-'
+
+                            try: 
+                                #fetch all saved favorites from favorites.json if exists
+                                file = open('favorites.json', "r")
+                                favorites = json.load(file)
+                                totalFavorites = len(favorites)
+                                # print("total favorites: " + str(totalFavorites))
+                                # print(favorites)
+                            except: 
+                                #create favorites.json if does not exists
+                                file = open('favorites.json', "w")
+                                file.close()
+
+                            #save new favorite
+                            newFavorite = {
+                                "id": str(cardId),
+                                "profile": profile, 
+                                "platform": platform, 
+                                "savedAt": str(datetime.datetime.now()), 
+                                "text": cardText
+                            }
+                            
+                            #check if already saved
+                            for f in favorites:
+                                if f['id'] == cardId:
+                                    print('news card already saved')
+                                    return
+                            
+                            #add new favorite
+                            favorites.append(newFavorite)
+                            out_file = open("favorites.json", "w")
+                            json.dump(favorites, out_file, indent = 6)
+                            out_file.close()
+
+                        elif s.text == '-':
+                            #change button style
+                            s.text = '+'
+                            s.background_color = "green"
+                            s.background_normal = "green"
+                            
+                            #fetch all saved favorites from favorites.json if exists
+                            file = open('favorites.json', "r")
+                            favorites = json.load(file)
+                            totalFavorites = len(favorites)
+                            # print("total favorites: " + str(totalFavorites))
+                            
+                            count = 0
+                            for f in favorites:
+                                #remove selected favorite
+                                if f['id'] == cardId:
+                                    favorites.pop(count)
+                                    out_file = open("favorites.json", "w")
+                                    json.dump(favorites, out_file, indent = 6)
+                                    out_file.close()
+                                count += 1
+                except:
+                    None
+            # print("")
+
+    def createNewsCard(self, text, newsType, profile):
+        #variables
+        baseHeight = 120
+        baseWidth = 660
+
+        #calculate card text height
+        tempTextLength = len(text)
+        textHeightMultiplier = int((tempTextLength) / 107) # one line of text 107 chars
+        textHeight = 37 * textHeightMultiplier # one line of text add 37 px
+        textHeightMinusCorrection = (textHeightMultiplier) * 20 # if multiplier is above 2 minus 20 px
+
+        #prints
+        # print("tempText count: " + str(tempTextLength))
+        # print("textHeightMultiplier: " + str(textHeightMultiplier))
+        # print("textHeightMinusCorrection: " + str(textHeightMinusCorrection))
+
+        #set total height
+        if textHeightMultiplier > 1:
+            totalHeight = baseHeight + textHeight - textHeightMinusCorrection
+        else:
+            totalHeight = baseHeight + textHeight
+        
+        #set total width
+        totalWidth = baseWidth
+
+        #boxlayout
+        bl = BoxLayout(orientation = "horizontal", size_hint_x = 1, size_hint_y = None)
+        bl.height = totalHeight - 40
+        bl.width = 660
+        bl.spacing = -2
+        bl.padding = 0
+
+        #fetch all saved favorites from favorites.json if exists
+        file = open('favorites.json', "r")
+        favorites = json.load(file)
+        totalFavorites = len(favorites)
+
+        #format text to id
+        btnIdFormatting1 = text.replace(" ", "")
+        btnIdFormatting2 = btnIdFormatting1.replace("_", "")
+        btnIdFormatting3 = btnIdFormatting2.replace("-", "")
+        btnIdFormatting4 = btnIdFormatting3.replace("\n", "")
+        btnIdFormatted = str(btnIdFormatting4[0:40])
+        
+        #check if id is already in favorites
+        if btnIdFormatted in str(favorites):
+            #create remove button
+            btn = Button(text = "-", size_hint_x = 0.1, size_hint_y = 1, background_color = 'darkred', background_normal = 'darkred', background_down = 'darkred')
+        elif btnIdFormatted not in str(favorites):
+            #create save button
+            btn = Button(text = "+", size_hint_x = 0.1, size_hint_y = 1, background_color = 'green', background_normal = 'green', background_down = 'green')
+        
+        #set additional save/remove button variables
+        btn.id = btnIdFormatted
+        btn.color = 'lightgray'
+        btn.bind(on_press=lambda *args: StartingScreen.saveToFavorites(self, btn.id, profile, newsType, text))
+
+        #news card text
+        b2 = Button(text = text)
+        b2.size_hint_y = None
+        b2.size_hint_x = 1
+        b2.padding = (20, 40)
+        b2.text_size = (610, totalHeight)
+        b2.height = totalHeight - 40
+        b2.multiline = True
+        b2.disabled = False
+        b2.halign = 'left'
+        b2.valign = 'top'
+        b2.color = 'lightgray'
+        b2.background_color = get_color_from_hex("#292f33")
+        b2.background_normal = 'transparent'
+        b2.background_down = 'transparent'
+        # if newsType == 'twitter':
+        #     b2.background_color = get_color_from_hex("#55acee")
+        #     b2.background_normal = 'transparent'
+        #     b2.background_down = 'transparent'
+        # elif newsType == 'youtube':
+        #     b2.background_color = get_color_from_hex("#e52d27")
+        #     b2.background_normal = 'transparent'
+        #     b2.background_down = 'transparent'
+
+        #add widgets to boxlayout
+        # bl.add_widget(lbl)
+        bl.add_widget(b2)
+        bl.add_widget(btn)
+
+        return bl
+
+    def createTitleCard(self, text, newsType):
+        #boxlayout
+        bl = BoxLayout(orientation = "horizontal", size_hint_x = 1, size_hint_y = None)
+        bl.height = 100
+        bl.width = 660
+
+        #save button
+        text = str(text).split("_")
+        formattedText = ""
+        for t in text:
+          formattedText += str(t).capitalize() + " "
+          
+        backgroundColor = get_color_from_hex("#292f33")
+        btn = Button(text = str(formattedText), size_hint_x = 1, size_hint_y = 1, background_color = backgroundColor, background_normal = 'transparent', background_down = 'transparent')
+        btn.color = 'lightgray'
+        btn.font_size = 30
+
+        bl.add_widget(btn)
+
+        return bl
 
 class AddProfileScreen(Screen):
     def __init__(self, **var_args):
@@ -614,7 +827,7 @@ class EditProfileScreen(Screen):
 class mainApp(App): #the Base Class of our Kivy App
     def build(self):
         #set window title
-        self.title = "Scraper News"
+        self.title = "Scraper News " + str(year_progress())
         
         #screen manager
         sm = ScreenManager()
